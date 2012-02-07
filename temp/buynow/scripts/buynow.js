6f28a5fc9bfb219b197bc0e1c4d4a866fb585443
Eric Jensen <jensen39@gmail.com>
2012-02-07 23:27:25 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@52 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- temp/buynow/scripts/buynow.js
+++ temp/buynow/scripts/buynow.js
@@ -2,34 +2,40 @@ $(function () {
 	var PAYPAL_LINK = 'https://www.e-junkie.com/ecom/gb.php?on0=D&os0=MIK&c=single&cl=1090&i=';
 	var GOOGLECHECKOUT_LINK = 'https://www.e-junkie.com/ecom/gb.php?&on0=D&os0=MIK&c=gc&cl=1090&ejc=4&i=';
 	var OS = {
-		Win: { Name: 'Digital Download for Windows 7 / XP / Vista', Code: 0 },
-		Mac: { Name: 'Digital Download for Mac OS X', Code: 2 }
-	};
-
-	var PN = {
-		Name: 'Platinum Notes 3.0',
-		Price: '98 USD',
-		Code: 20,
-		Description: 'Platinum Notes improves your files and gives them perfect volume and correct pitch.  It removes distortion and clipped peaks.  100% automated and designed for top DJs.'
+		Win: { Description: 'Digital Download for Windows 7 / XP / Vista', Code: 0 },
+		Mac: { Description: 'Digital Download for Mac OS X', Code: 2 }
 	};
 
 	var MIK = {
 		Name: 'Mixed In Key 5.0',
-		Price: '58 USD',
-		Code: 100,
+		Price: 58,
+		BundleCode: 10,
 		Description: 'Mixed In Key is award-winning DJ software that gives you the #1 technique of the world\'s best DJ\'s - harmonic mixing.'
 	};
 
-	var options = {
-		Product: 'Mashup Version 1.0',
+	var PN = {
+		Name: 'Platinum Notes 3.0',
+		Price: 98,
+		BundleCode: 20,
+		Description: 'Platinum Notes improves your files and gives them perfect volume and correct pitch.  It removes distortion and clipped peaks.  100% automated and designed for top DJs.'
+	};
+
+	var Mashup = {
+		Name: 'Mashup Version 1.0',
+		Price: 39,
 		Code: 1200,
-		Price: '$39 US Dollars',
+		BundleCode: 30,
+		Description: 'Platinum Notes improves your files and gives them perfect volume and correct pitch.  It removes distortion and clipped peaks.  100% automated and designed for top DJs.'
+	};
+
+	var options = {
+		Product: Mashup,
 		BannerImage: 'images/shoppingCartTop.png',
-		Bundles: [MIK, PN] // Choose which bundles to display here.
+		Bundles: [MIK, PN] // Choose which bundles to display.
 	};
 
 	function onBundleChanged() {
-		var code = options.Code + options.OS.Code;
+		var code = options.Product.Code + options.OS.Code;
 		$('.bundle_checkbox').each(function () {
 			var $checkBox = $(this);
 			if ($checkBox.is(':checked')) {
@@ -43,7 +49,6 @@ $(function () {
 
 	function buyNowButtonClicked(e, os) {
 		options.OS = os;
-		options.SubProduct = os.Name;
 
 		$('#buyNowTemplate').tmpl(options).lightbox_me({ centered: true, closeClick: false, lightboxSpeed: 150, overlaySpeed: 150, destroyOnClose: true });
 
