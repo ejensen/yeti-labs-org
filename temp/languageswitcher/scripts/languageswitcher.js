984360ef217404999c2f1830b791b9e36d07ce6b
Eric Jensen <jensen39@gmail.com>
2012-02-09 05:21:15 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@64 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- temp/languageswitcher/scripts/languageswitcher.js
+++ temp/languageswitcher/scripts/languageswitcher.js
@@ -2,17 +2,14 @@
 	var language_switcher = document.getElementById('language_switcher');
 	
 	language_switcher.onchange = function () {
-		// e.g. http://[domain]/[path]/ChangeLanguage/?lang=[lang]
-		var baseUrl = location.protocol + '//' + location.host + window.location.pathname;
-		if (baseUrl[baseUrl.length - 1] !== '/') {
-			baseUrl += '/';
-		}
-		window.location.replace(baseUrl + 'ChangeLanguage?lang=' + language_switcher.options[language_switcher.selectedIndex].value);
-		
-		/*
-		// e.g. http://[domain]/ChangeLanguage/?lang=[lang]&path=[path]
-		var baseUrl = location.protocol + '//' + location.host;
-		window.location.replace(baseUrl + 'ChangeLanguage/?lang=' + language_switcher.options[language_switcher.selectedIndex].value + '&path=' + window.location.pathname);
-		*/
+		var pattern = '${host}/${path}/ChangeLanguage/?lang=${lang}';
+		//            '${host}/ChangeLanguage/?lang=${lang}&path=${path}';
+
+		window.location.replace(window.location.protocol + '//' +
+								pattern.replace('${host}', window.location.host)
+								.replace('${path}', window.location.pathname)
+								.replace('${lang}', language_switcher.options[language_switcher.selectedIndex].value)
+								.replace(/\/\//g, '/')
+							);
 	};
 }());
