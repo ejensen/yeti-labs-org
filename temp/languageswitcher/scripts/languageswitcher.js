17168d25d92beb73e9d33c78581e0bfa8c4c6f08
Eric Jensen <jensen39@gmail.com>
2012-02-09 04:28:25 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@62 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- temp/languageswitcher/scripts/languageswitcher.js
+++ temp/languageswitcher/scripts/languageswitcher.js
@@ -2,7 +2,7 @@
 	var language_switcher = document.getElementById('language_switcher');
 	
 	language_switcher.onchange = function () {
-		// e.g. http://mashup.mixedinkey.com/[path]/ChangeLanguage/?lang=[lang]
+		// e.g. http://[domain]/[path]/ChangeLanguage/?lang=[lang]
 		var baseUrl = location.protocol + '//' + location.host + window.location.pathname;
 		if (baseUrl[baseUrl.length - 1] !== '/') {
 			baseUrl += '/';
@@ -10,7 +10,7 @@
 		window.location.replace(baseUrl + 'ChangeLanguage?lang=' + language_switcher.options[language_switcher.selectedIndex].value);
 		
 		/*
-		// e.g. http://mashup.mixedinkey.com/ChangeLanguage/?lang=[lang]&path=[path]
+		// e.g. http://[domain]/ChangeLanguage/?lang=[lang]&path=[path]
 		var baseUrl = location.protocol + '//' + location.host;
 		window.location.replace(baseUrl + 'ChangeLanguage/?lang=' + language_switcher.options[language_switcher.selectedIndex].value + '&path=' + window.location.pathname);
 		*/
