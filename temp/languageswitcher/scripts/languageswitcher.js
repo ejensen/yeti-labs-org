15586f2a9921bc6e144672716026a7efafd7cd3c
Eric Jensen <jensen39@gmail.com>
2012-02-09 23:49:26 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@66 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- temp/languageswitcher/scripts/languageswitcher.js
+++ temp/languageswitcher/scripts/languageswitcher.js
@@ -1,15 +1,49 @@
 (function () {
+	var LANGUAGE_KEY = 'user_language';
+
+	function getCookie(name) {
+        var pairs = document.cookie.split('; ');
+		var i, pair;
+        for (i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
+            if (decodeURIComponent(pair[0]) === name)
+				return decodeURIComponent(pair[1] || '');
+        }
+        return null;
+	}
+
+	function setCookie(name, value, expires, path, domain, secure){
+		var today = new Date();
+		today.setTime(today.getTime());
+
+		if (expires) {
+			var expiresDate = new Date();
+			expiresDate.setDate(expiresDate.getDate() + expires);
+		}
+
+		document.cookie = name + '=' + escape(value) +
+		(expires ? ';expires=' + expiresDate.toUTCString() : '') +
+		(path    ? ';path=' + path : '') +
+		(domain  ? ';domain=' + domain : '') +
+		(secure  ? ';secure' : '');	
+	}
+
 	var language_switcher = document.getElementById('language_switcher');
 	
-	language_switcher.onchange = function () {
-		var pattern = '${host}/${path}/ChangeLanguage/?lang=${lang}';
-		//            '${host}/ChangeLanguage/?lang=${lang}&path=${path}';
+	var currentLanguage = getCookie(LANGUAGE_KEY);
+	if (currentLanguage) {
+		var i;
+		for (i = 0; i < language_switcher.length; i++) {
+			if (language_switcher[i].value === currentLanguage) {
+				language_switcher.selectedIndex = i;
+				break;
+			}
+		}
+	}
 
-		window.location.replace(window.location.protocol + '//' +
-								pattern.replace('${host}', window.location.host)
-								.replace('${path}', window.location.pathname)
-								.replace('${lang}', language_switcher.options[language_switcher.selectedIndex].value)
-								.replace(/\/\//g, '/')
-							);
+	language_switcher.onchange = function () {
+		var lang = language_switcher.options[language_switcher.selectedIndex].value;
+		setCookie(LANGUAGE_KEY, lang, undefined, '/');
+		
+		window.location.reload(true);
 	};
 }());
