371b8c0405d6a3870997e593c49443b0ed0fd0ed
Eric Jensen <jensen39@gmail.com>
2012-02-09 04:26:39 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@61 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- temp/languageswitcher/scripts/languageswitcher.js
+++ temp/languageswitcher/scripts/languageswitcher.js
@@ -2,10 +2,17 @@
 	var language_switcher = document.getElementById('language_switcher');
 	
 	language_switcher.onchange = function () {
+		// e.g. http://mashup.mixedinkey.com/[path]/ChangeLanguage/?lang=[lang]
 		var baseUrl = location.protocol + '//' + location.host + window.location.pathname;
 		if (baseUrl[baseUrl.length - 1] !== '/') {
 			baseUrl += '/';
 		}
 		window.location.replace(baseUrl + 'ChangeLanguage?lang=' + language_switcher.options[language_switcher.selectedIndex].value);
+		
+		/*
+		// e.g. http://mashup.mixedinkey.com/ChangeLanguage/?lang=[lang]&path=[path]
+		var baseUrl = location.protocol + '//' + location.host;
+		window.location.replace(baseUrl + 'ChangeLanguage/?lang=' + language_switcher.options[language_switcher.selectedIndex].value + '&path=' + window.location.pathname);
+		*/
 	};
 }());
