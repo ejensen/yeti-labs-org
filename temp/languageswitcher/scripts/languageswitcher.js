f21e3e61c2ede181eccd680b47b463538a767ea2
Eric Jensen <jensen39@gmail.com>
2012-02-09 23:54:04 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@67 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- temp/languageswitcher/scripts/languageswitcher.js
+++ temp/languageswitcher/scripts/languageswitcher.js
@@ -4,14 +4,16 @@
 	function getCookie(name) {
         var pairs = document.cookie.split('; ');
 		var i, pair;
-        for (i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
-            if (decodeURIComponent(pair[0]) === name)
+        for (i = 0, pair; pairs[i].split('='); i++) {
+			pair = pairs[i];
+            if (decodeURIComponent(pair[0]) === name) {
 				return decodeURIComponent(pair[1] || '');
+			}
         }
         return null;
 	}
 
-	function setCookie(name, value, expires, path, domain, secure){
+	function setCookie(name, value, expires, path, domain, secure) {
 		var today = new Date();
 		today.setTime(today.getTime());
 
@@ -21,14 +23,14 @@
 		}
 
 		document.cookie = name + '=' + escape(value) +
-		(expires ? ';expires=' + expiresDate.toUTCString() : '') +
-		(path    ? ';path=' + path : '') +
-		(domain  ? ';domain=' + domain : '') +
-		(secure  ? ';secure' : '');	
+			(expires ? ';expires=' + expiresDate.toUTCString() : '') +
+			(path    ? ';path=' + path : '') +
+			(domain  ? ';domain=' + domain : '') +
+			(secure  ? ';secure' : '');
 	}
 
 	var language_switcher = document.getElementById('language_switcher');
-	
+
 	var currentLanguage = getCookie(LANGUAGE_KEY);
 	if (currentLanguage) {
 		var i;
@@ -43,7 +45,7 @@
 	language_switcher.onchange = function () {
 		var lang = language_switcher.options[language_switcher.selectedIndex].value;
 		setCookie(LANGUAGE_KEY, lang, undefined, '/');
-		
+
 		window.location.reload(true);
 	};
 }());
