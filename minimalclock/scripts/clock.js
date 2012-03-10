5cad2a8bb77d18a24a8d715daeb7f89d6a44e136
Eric Jensen <jensen39@gmail.com>
2012-03-10 11:48:02 +0000

    git-svn-id: file:///Users/Eric/Developer/Other/YetiLabsSVN@236 314f18ea-a15e-482d-8d01-371d8c3e8fc5

--- minimalclock/scripts/clock.js
+++ minimalclock/scripts/clock.js
@@ -1,41 +1,41 @@
 (function() {
 	var started = false;
-	var previewArea = document.getElementById('preview-area');
+	var preview = document.getElementById('preview');
+
+	function toTwoDigitString(number) {
+		return (number < 10 ? '0' : '') + number;
+	}
 
 	function updateTime() {
 		var time = new Date(),
-			hours = time.getHours(),
-			minutes = time.getMinutes(),
-			seconds = time.getSeconds();
-
-		hours   = (hours   < 10 ? '0' : '') + hours;
-		minutes = (minutes < 10 ? '0' : '') + minutes;
-		seconds = (seconds < 10 ? '0' : '') + seconds;
+			hours = toTwoDigitString(time.getHours()),
+			minutes = toTwoDigitString(time.getMinutes()),
+			seconds = toTwoDigitString(time.getSeconds());
 
 		var timeString = hours + ':' + minutes + ':' + seconds;
 
 		document.getElementById('time').innerHTML = timeString;
 	}
-	
+
 	function startClock() {
 		if (!started) {
 			started = true;
 			document.getElementById('screen').style.display = 'block';
 			updateTime();
 			setInterval(updateTime, 1000);
-			previewArea.style.cursor = 'auto';
+			preview.style.cursor = 'auto';
 
-			if (previewArea.removeEventListener) {
-				previewArea.removeEventListener('click', startClock); 
-			} else if (previewArea.detachEvent)  {
-				previewArea.detachEvent ('click', startClock);
+			if (preview.removeEventListener) {
+				preview.removeEventListener('click', startClock); 
+			} else if (preview.detachEvent)  {
+				preview.detachEvent('click', startClock);
 			}
 		}
 	}
 	
-	if (previewArea.addEventListener) {
-		previewArea.addEventListener('click', startClock, false); 
-	} else if (previewArea.attachEvent)  {
-		previewArea.attachEvent('onclick', startClock);
+	if (preview.addEventListener) {
+		preview.addEventListener('click', startClock, false); 
+	} else if (preview.attachEvent)  {
+		preview.attachEvent('onclick', startClock);
 	}
 })();
